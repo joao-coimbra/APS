2ce5a41936ec113032c31def23f7446fb340a0ff
João Coimbra <joao.benatti.coimbra@gmail.com>
2020-05-22 21:07:06 -0300

    APS base

--- README.md
+++
@@ -1,2 +0,0 @@
-# APS
-Trabalho do curso
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+function showPress() {
+    document.getElementById("pdrop").style.opacity = "1";
+}
+
+function hiddenPress() {
+    document.getElementById("pdrop").style.opacity = "0";
+}
