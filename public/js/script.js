66ffc5a0574dc44a42cd00360dc8f2e1e6854b7c
João Coimbra <joao.benatti.coimbra@gmail.com>
2020-05-23 19:27:57 -0300

    Ajustes no código

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+$('main a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+
 function showPress() {
     document.getElementById("pdrop").style.opacity = "1";
 }
