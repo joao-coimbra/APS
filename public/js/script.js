f47a9afd6df5cab7daf1d474f6b70176ee70284a
João Coimbra <joao.benatti.coimbra@gmail.com>
2020-05-24 02:04:18 -0300

    Inserindo navbar lateral

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,64 @@
-$('main a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
-});
-
 function showPress() {
-    document.getElementById("pdrop").style.opacity = "1";
+	document.getElementById("pdrop").style.opacity = "1";
 }
 
 function hiddenPress() {
-    document.getElementById("pdrop").style.opacity = "0";
+	document.getElementById("pdrop").style.opacity = "0";
+}
+
+function alterarEstiloMenu(num) {
+	
+	let el;
+
+	switch (num) {
+		case 1:
+			el = document.getElementById("linkHome");
+
+			if (el.style.color == "green") {
+				el.style.color = "black";
+			}
+			else {
+				el.style.color = "green";
+			}
+			break;
+			
+		case 2:
+			el = document.getElementById("linkReciclo");
+
+			if (el.style.color == "green") {
+				el.style.color = "black";
+			}
+			else {
+				el.style.color = "green";
+			}
+			break;
+			
+		case 3:
+			el = document.getElementById("linkAgua");
+
+			if (el.style.color == "green") {
+				el.style.color = "black";
+			}
+			else {
+				el.style.color = "green";
+			}
+			break;
+
+		case 4:
+			el = document.getElementById("linkOutros");
+
+			if (el.style.color == "green") {
+				el.style.color = "black";
+			}
+			else {
+				el.style.color = "green";
+			}
+			break;
+
+		default:
+			break;
+	}
+
+
+
 }
